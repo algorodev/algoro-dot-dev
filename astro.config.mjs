5ca5db6baa0fffc73ced756f88ffc4027b2e5e50
Alex <me@algoro.dev>
2025-09-11 19:36:02 +0100

    feat: added sitemap and meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://www.algoro.dev',
+  output: 'static',
+  trailingSlash: 'never',
+  integrations: [mdx(), sitemap()],
+  prefetch: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
         '@assets': new URL('./src/assets', import.meta.url).pathname,
         '@components': new URL('./src/components', import.meta.url).pathname,
+        '@content': new URL('./src/content', import.meta.url).pathname,
         '@layouts': new URL('./src/layouts', import.meta.url).pathname,
         '@lib': new URL('./src/lib', import.meta.url).pathname,
         '@pages': new URL('./src/pages', import.meta.url).pathname,
